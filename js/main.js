55d250ef76a43daaa1c67429e4bac85cec21c4d8
Marc Martinez <mxrcxs.mxrtxnez@gmail.com>
2022-04-29 16:13:24 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function parrafos() {
+    var infonum = document.getElementById('infonum')
+    infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
+    var infonum2 = document.getElementById('infonum2')
+    if (numero << 1) {
+        var infonum2 = 'Here is your decimal'
+    }
+}
