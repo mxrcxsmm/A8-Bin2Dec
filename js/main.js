6784d8ee895fa91177ee061d8473d516d3e8ff1f
Marc Martinez <mxrcxs.mxrtxnez@gmail.com>
2022-05-01 20:27:10 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-function converto() {
+function parrafos() {
     var infonum = document.getElementById('infonum')
-    infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
     var infonum2 = document.getElementById('infonum2')
-    if (numero << 1) {
-        var infonum2 = 'Here is your decimal'
+    var numero = document.getElementById('numero').value
+    var calculando = document.getElementById('calculando')
+    calculando.innerHTML = parseInt(numero, 2);
+    var ultimodigito = numero.charAt(numero.length - 1)
+    if (numero == '') {
+        calculando.innerHTML = ''
+    }
+    if (ultimodigito == 0 || ultimodigito == 1) {
+        infonum2.innerHTML = 'Here is your decimal'
+        infonum.innerHTML = ''
+    } else {
+        calculando.innerHTML = 'Waiting for a valid binary number...'
+    }
+    if (ultimodigito >> 1 || ultimodigito >> 9999999) {
+        infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
+        infonum2.innerHTML = ''
+    }
+    if (ultimodigito == '') {
+        infonum.innerHTML = ''
+        infonum2.innerHTML = ''
     }
 }
